b5d5f2b97a45715a68a88b09a79e16efc6c6b3f1
Vivekanand <th0ratvivekaa@gmail.com>
2023-03-04 17:00:31 +0530

    nav1 commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -257,7 +257,7 @@ const Navbar = () => {
                 <p className={Styles.navText}>How to Reach</p>
               </li>
               <li>
-                <p className={Styles.navText}>Contact Us</p>
+                <p className={Styles.navText}>Contact</p>
               </li>
             </ul>
           )}
